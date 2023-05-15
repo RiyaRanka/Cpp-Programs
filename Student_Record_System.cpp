e3054c6c37cd1cfdf45dfe2ec48598c045c5a514
RiyaRanka <riyaranka2017@gmail.com>
2023-05-15 17:02:10 +0530

    Added Info about project

--- Student_Record_System.cpp
+++ Student_Record_System.cpp
@@ -2,7 +2,7 @@
 // Make a table by inputing the data from user about a student
 // Ask the user about this information first: 
 // Enter the Scholar no(unique), Full Name of Student, Branch, Current Year, Current Sem (Exceptional can display the table with unique branch differently)
-// Menu -> Add the info about student 1)Name, 2)Scholar no. , 3)Branch, 4)Current year, 5) Current sem, Edit anything all four, Search about student with all four (if possible otherwise only name or scholar no.), Update all four (the scholar no. will always be unique to every student), Delete the info about a student, Show or Print the table of Student Record
+// Menu -> Add the info about student 1)Name, 2)Scholar no. , 3)Branch, 4)Current year, 5) Current sem, {6) Last sem sgpa or cgpa} Edit anything all four, Search about student with all four (if possible otherwise only name or scholar no.), Update all four (the scholar no. will always be unique to every student), Delete the info about a student, Show or Print the table of Student Record
 // run in while loop until user exit from the program
 //  If c++ have a special feature like accessing c++ files to a single file in the main file so that the project doesn't looke heavy
 // DAY 1 Progress (15/5): Simple record system of student add input function & Stored the input information by user
