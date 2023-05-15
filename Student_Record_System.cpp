7000fe02ccb65db62274cc7823e475e5853bfaca
RiyaRanka <riyaranka2017@gmail.com>
2023-05-15 16:40:18 +0530

    Added Info about project

--- Student_Record_System.cpp
+++ Student_Record_System.cpp
@@ -4,3 +4,8 @@
 // Enter the Scholar no(unique), Full Name of Student, Branch, Current Year, Current Sem (Exceptional can display the table with unique branch differently)
 // Menu -> Add the info about student 1)Name, 2)Scholar no. , 3)Branch, 4)Current year, 5) Current sem, Edit anything all four, Search about student with all four (if possible otherwise only name or scholar no.), Update all four (the scholar no. will always be unique to every student), Delete the info about a student, Show or Print the table of Student Record
 // run in while loop until user exit from the program
+//  If c++ have a special feature like accessing c++ files to a single file in the main file so that the project doesn't looke heavy
+// DAY 1 Progress (15/5): Simple record system of student add input function & Stored the input information by user
+// DAY 2 Progress (16/5):
+// DAY 3 Progress (17/5): 
+// DAY 4 Progress (18/5): 
