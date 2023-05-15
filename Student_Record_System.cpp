a2dbf982fcc2a5b915db865f745169e5218bd08b
RiyaRanka <riyaranka2017@gmail.com>
2023-05-15 16:36:46 +0530

    Added Info about project

---
+++ Student_Record_System.cpp
@@ -0,0 +1,6 @@
+// Make a project regarding Student Record System in which you can Read, Update or Modify & Delete or Edit the operation
+// Make a table by inputing the data from user about a student
+// Ask the user about this information first: 
+// Enter the Scholar no(unique), Full Name of Student, Branch, Current Year, Current Sem (Exceptional can display the table with unique branch differently)
+// Menu -> Add the info about student 1)Name, 2)Scholar no. , 3)Branch, 4)Current year, 5) Current sem, Edit anything all four, Search about student with all four (if possible otherwise only name or scholar no.), Update all four (the scholar no. will always be unique to every student), Delete the info about a student, Show or Print the table of Student Record
+// run in while loop until user exit from the program
