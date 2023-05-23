8a6027afc07da8f0d3f33a97e558e9ff3c5718ae
RiyaRanka <riyaranka2017@gmail.com>
2023-05-23 22:16:20 +0530

    Reverse Array updated

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void reverseArray (int arr[], int size) {
     int start = 0, end = size-1;
     while (start <= end) {
-        swap (arr[start], arr[end]); // how does swap function works
+        swap (arr[start], arr[end]); 
         start++;
         end--;
         }
@@ -15,16 +15,17 @@ void printReverse(int arr[], int size) {
         cout << arr[i] << " ";
     }
     cout << endl;
-}
+} 
 
 int main() {
     int size, arr[50], i;
     cout << "Enter size of array: ";
     cin >> size;
     for (i=0; i<size; i++) {
-        cout << "Value of array " << i+1 << " is ";
+        cout << "Value of array at index " << i << " is ";
         cin >> arr[i];
     }
+    cout << endl;
 
     cout << "Your array is ";
     for (i =0; i<size; i++) {
