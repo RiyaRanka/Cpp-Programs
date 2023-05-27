a604b25fe1019d3584c5e7f46d1d481fbc2b550e
Riya Ranka <81897389+RiyaRanka@users.noreply.github.com>
2023-05-27 17:51:06 +0530

    Added array_compare.cpp

---
+++ array_compare.cpp
@@ -0,0 +1,106 @@
+/*
+Problem Statement:
+
+Write a C program that takes two arrays as input and determines whether they are equal or not. 
+The program should compare the elements of both arrays and return true if they have the same elements in any order, and false otherwise.
+
+Inputs:
+
+Two arrays A[] and B[] of integers.
+The size of the arrays N, where N represents the number of elements in each array.
+Output:
+True if the arrays A[] and B[] are equal, false otherwise.
+
+Example:
+-------------
+Input:
+N = 3
+A[] = {1, 2, 5}
+B[] = {2, 5, 1}
+
+Output:
+True
+-------------
+Input:
+A[] = {1, 2, 5, 1}
+B[] = {2, 5, 1, 2}
+
+Output:
+False
+-------------
+Input:
+A[] = {1, 2, 5, 1}
+B[] = {2, 5, 1, 5}
+
+Output:
+False
+-------------
+Note:
+
+- The program should handle arrays of any size.
+- The order of elements in the arrays does not matter for determining equality.
+- The program should provide the correct output for all possible valid inputs.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int compareArray(int size1, int arr1[], int size2, int arr2[]) {
+    int i, j, count = 0, flag = 0;
+    for (i=0; i<size1; i++) {
+        for (j=0; j<size2; j++) {
+            if (arr1[i] == arr2[j]) {
+               flag = 1;
+                arr2[j] = arr2[size2 - 1];
+                size2--;
+               count++;
+               break;
+            }
+            else {
+                flag = 0;
+            }
+        }
+    }
+    if (flag == 0) {
+        flag++;
+        cout << "False" << endl;
+    }
+    if (count == size1 || count == size2) {
+        count++;
+        cout << "True" << endl;
+    }
+
+    return 0;
+}
+
+int main() {
+    int size1, arr1[50], size2, arr2[50];
+    cout << "Enter size of 1st array: ";
+    cin >> size1;
+    int i1, i2;
+    for (i1=0; i1<size1; i1++) {
+        cout << "Enter value of 1st array : " ;
+        cin >> arr1[i1];
+    }
+    
+    cout << "\nEnter size of 2nd array: ";
+    cin >> size2;
+    for (i2=0; i2<size2; i2++) {
+        cout << "Enter value of 2nd array : ";
+        cin >> arr2[i2];
+    }
+
+    cout << "\nFirst array : [";
+    for (i1=0; i1<size1; i1++) {
+        cout << arr1[i1] << ", ";
+    }
+    cout << "\b\b]" << endl;
+    cout << "Second array : [";
+    for (i2=0; i2<size2; i2++) {
+        cout << arr2[i2] << ", ";
+    }
+    cout << "\b\b]" << endl;
+    compareArray (size1, arr1, size2, arr2);
+    // reversePrint (arr1, size1, arr2, size2);
+}
