60ec95a1dd5e9572c0e8f04ad3b191e53afda879
Riya Ranka <81897389+RiyaRanka@users.noreply.github.com>
2023-05-27 22:44:39 +0530

    Updated array_compare.cpp

--- array_compare.cpp
+++ array_compare.cpp
@@ -1,46 +1,11 @@
 /*
 Problem Statement:
-
 Write a C program that takes two arrays as input and determines whether they are equal or not. 
 The program should compare the elements of both arrays and return true if they have the same elements in any order, and false otherwise.
 
-Inputs:
-
-Two arrays A[] and B[] of integers.
-The size of the arrays N, where N represents the number of elements in each array.
-Output:
-True if the arrays A[] and B[] are equal, false otherwise.
-
-Example:
--------------
-Input:
-N = 3
-A[] = {1, 2, 5}
-B[] = {2, 5, 1}
-
-Output:
-True
--------------
-Input:
-A[] = {1, 2, 5, 1}
-B[] = {2, 5, 1, 2}
-
-Output:
-False
--------------
-Input:
-A[] = {1, 2, 5, 1}
-B[] = {2, 5, 1, 5}
-
-Output:
-False
--------------
-Note:
-
-- The program should handle arrays of any size.
-- The order of elements in the arrays does not matter for determining equality.
-- The program should provide the correct output for all possible valid inputs.
-
+Input: Two arrays arr1[] and arr2[] of integers.
+The size of the arrays size1 and size2, where size1, size2 represents the number of elements in arr1 and arr2 respectively.
+Output: True if the arrays arr1[] and arr2[] are equal, false otherwise.
 */
 
 #include <iostream>
@@ -70,7 +35,6 @@ int compareArray(int size1, int arr1[], int size2, int arr2[]) {
         count++;
         cout << "True" << endl;
     }
-
     return 0;
 }
 
@@ -102,5 +66,5 @@ int main() {
     }
     cout << "\b\b]" << endl;
     compareArray (size1, arr1, size2, arr2);
-    // reversePrint (arr1, size1, arr2, size2);
+    return 0;
 }
