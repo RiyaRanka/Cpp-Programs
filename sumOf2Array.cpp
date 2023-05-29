9bcb1d2685221eb2c01ac2c6d4f14fd59d15b037
Riya Ranka <81897389+RiyaRanka@users.noreply.github.com>
2023-05-29 17:26:15 +0530

    Update sumOf2Array.cpp

--- sumOf2Array.cpp
+++ sumOf2Array.cpp
@@ -1,3 +1,4 @@
+// Problem: Sum of two array by parallel indexing, both arrays ar inputed by user.
 #include <iostream>
 using namespace std;
 int main() {
@@ -5,17 +6,19 @@ int main() {
     cout << "Enter 1st array size: ";
     cin >> size1;
     int arr1[100];
+    cout << "Enter value of 1st array : ";
     for (i=0; i<size1; i++) {
-        cout << "Enter value of 1st array at index " << i << ": ";
         cin >> arr1[i];
     }
+    
     cout << "Enter 2nd array size: ";
     cin >> size2;
     int arr2[100];
+    cout << "Enter value of 2nd array : ";
     for (i=0; i<size2; i++) {
-        cout << "Enter value of 2nd array at index " << i << ": ";
         cin >> arr2[i];
     }
+    
     if(size1<size2){
         while(size1<size2){
             arr1[size1] =0;
@@ -33,31 +36,11 @@ int main() {
         sum[i] =arr1[i]+arr2[i];
 
     }
-    cout<<"\nThe sum of parallel index is :";
+    cout<<"\nThe sum of parallel index is : [";
     for(int i=0;i<size1;i++){
-    cout << sum[i] << " " << endl;
+    cout << sum[i] << ", ";
     }
+    cout << "\b\b]" << endl;
 
-    // int result[100];
-    // int n1 = sizeof(arr1)/sizeof(arr1[i]);
-    // int n2 = sizeof(arr2)/sizeof(arr2[i]);
-    // if (n1>n2) {
-    //     int result[size1];
-    //     for (i=0; i<size1; i++) {
-    //         result[size1] = arr1[size1] + arr2[size2];
-    //         cout << "Sum of two arrays " << result[size1] << " ";
-    //     }
-    // }
-   
-    //     int result[n2];
-    //     result[n2] = arr1[size1] + arr2[size2];
-    //     cout << "Sum of two arrays " << result[n2];
-    // }
-
-    // for (i=0; i<size1; i++) {
-    //     int result[arr1[size1] + arr2[size2]];
-    //     // result[size1] = arr1[size1] + arr2[size2];
-    //     cout << "Sum of two arrays " << result[arr1[size1] + arr2[size2]] << " ";
-    // }
     return 0;
 }
