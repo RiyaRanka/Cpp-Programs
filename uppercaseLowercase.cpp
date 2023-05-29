0a91cffb6cf9bb307afb36aa1626dbea7503238a
Riya Ranka <81897389+RiyaRanka@users.noreply.github.com>
2023-05-29 17:35:04 +0530

    Update uppercaseLowercase.cpp

--- uppercaseLowercase.cpp
+++ uppercaseLowercase.cpp
@@ -1,10 +1,11 @@
+// Problem: Convert uppercase to lowercase and lowercase to uppercase.
 #include <iostream>
 using namespace std;
 int main () {
-     string s1 = "abcde";
+     string s1 = "riya";
     transform(s1.begin(), s1.end(), s1.begin(), ::toupper);
     cout<<s1<<endl;
-    string s2 = "WXYZ"
+    string s2 = "RIYA"
     transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
    cout<<s2<<endl;   
     return 0;
