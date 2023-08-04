d37fafefe5e99913faeabe1c8999c05fca3a2bf4
Riya Ranka <81897389+RiyaRanka@users.noreply.github.com>
2023-08-04 11:25:42 +0530

    Update sorting_array.cpp

--- sorting_array.cpp
+++ sorting_array.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
+    int n, arr[100];
     cout << "Size of array = ";
     cin >> n;
-    int arr[n];
     cout << "Give an array: ";
-    for(int i=0; i<n; i++) 
+    for(int i=0; i<n; i++) {
         cin>>arr[i];
-    for(int i=0; i<n; i++){ 
-        for(int j=i+1; j<n; j++) { if(arr[i]>arr[j]){
+    }
+    for(int i=0; i<n; i++) { 
+        for(int j=i+1; j<n; j++) { 
+            if(arr[i] > arr[j]) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
